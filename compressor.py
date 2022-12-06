3b64364ac80ff4abcf6998c1e6c505bed6623ec5
xcaim04 <87400766+CarlosJavier4@users.noreply.github.com>
2022-12-05 20:51:41 -0500

    Update compressor.py

--- compressor.py
+++ compressor.py
@@ -1,3 +1,14 @@
 import sys
 import os
-from PIL import Imagen
+from PIL import Image
+
+def compressor(directory):
+	for file in os.listdir(directory):
+		nameFile, extension = os.path.splitext(directory + file)
+
+		if extension in ['.jpg', '.png', '.jpeg']:
+			aux = Image.open(directory + file)
+			aux.save(directory + '__compressed__' + file, optimize=True, quality=60)
+
+if __name__ == '__main__':
+	compressor(sys.argv[1])
